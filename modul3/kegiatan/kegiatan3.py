322aba2043d479b3ab929d0fe7a78e3a770654f2
Muhammad Nizar <mnizarzr@gmail.com>
2023-11-01 15:03:53 +0700

    3.3 make it more succint

--- modul3/kegiatan/kegiatan3.py
+++ modul3/kegiatan/kegiatan3.py
@@ -1,25 +1,12 @@
 def map_to_units_tens_hundreds(digit, place):
     return {place: int(digit)}
 
-
-def is_integer(x):
-    return isinstance(x, int)
-
-
-def is_float(x):
-    return isinstance(x, float)
-
-
-def is_str(x):
-    return isinstance(x, str)
-
-
 random_list = [105, 3.1, "Hello", 737, "Python", 2.7, "World", 412, 5.5, "AI"]
 int_dict = {}
-float_tup = tuple(filter(is_float, random_list))
-str_list = list(filter(is_str, random_list))
+float_tup = tuple(filter(lambda x : isinstance(x, float), random_list))
+str_list = list(filter(lambda x : isinstance(x, str), random_list))
 
-for i in filter(is_integer, random_list):
+for i in filter(lambda x : isinstance(x, int), random_list):
     item_str = str(i)
     place_values = ["ratusan", "puluhan", "satuan"]
     i_dict = {}
